665c93cb6e1f82b9c7d92d1bff58f017cea869df
John Doll <jdollmu@gmail.com>
2025-03-21 00:00:10 -0400

    clean up scripts

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -9,17 +9,15 @@ print(f'Found {len(stats_files)} files in the stats folder')
 # Order the stats files
 stats_files.sort()
 
+# read data into data frame and store in map
 data_frame = {}
-
 for file in stats_files:
     data = pd.read_csv(f'./clean_data/stats/{file}')
     data_frame[file.split('.')[0]] = data
 
-# Drop the first two columns
+# Drop the first two columns, we don't need the specific team ranks or names for this analysis
 for frame in data_frame:
     data_frame[frame] = data_frame[frame].drop(data_frame[frame].columns[[0, 1]], axis=1)
-
-print(data_frame)
 
 # Rearrange data into a table for SM and a table FP storing just the max value from each year
 sm_data = []
@@ -30,28 +28,23 @@ for frame in data_frame:
     if year == '2020':
         continue
     min_values = data_frame[frame].min()
-    winning_values = data_frame[frame].max()
-    winning_values['Year'] = year
-    # we want the max values for call categories except for 'WHIP' and 'ERA'
-    winning_values['WHIP'] = min_values['WHIP']
-    winning_values['ERA'] = min_values['ERA']
+    winning_values_by_cat = data_frame[frame].max()
+    winning_values_by_cat['Year'] = year
+    # we want the max values for call categories except for 'WHIP' and 'ERA', where the lowest value wins
+    winning_values_by_cat['WHIP'] = min_values['WHIP']
+    winning_values_by_cat['ERA'] = min_values['ERA']
     if 'SM' in frame:
-        sm_data.append(winning_values)
+        sm_data.append(winning_values_by_cat)
     else:
-        fp_data.append(winning_values)
+        fp_data.append(winning_values_by_cat)
 
-# Convert lists to DataFrames, but make sure to include the 'Year' column first
+# Convert lists to DataFrames
 sm_df = pd.DataFrame(sm_data)
-sm_df = sm_df[['Year'] + [col for col in sm_df.columns if col != 'Year']]
 fp_df = pd.DataFrame(fp_data)
+
+# Put the 'Year' column first
+sm_df = sm_df[['Year'] + [col for col in sm_df.columns if col != 'Year']]
 fp_df = fp_df[['Year'] + [col for col in fp_df.columns if col != 'Year']]
-
-# Print the DataFrames
-print("SM DataFrame:")
-print(sm_df.to_string(index=False))
-
-print("FP DataFrame:")
-print(fp_df.to_string(index=False))
 
 # Save the DataFrames to CSV files
 # sm_df.to_csv('./results/sm_max_values.csv', index=False)
@@ -62,12 +55,14 @@ winner_avg = {}
 for col in sm_df.columns:
     if col == 'Year':
         continue
-    winner_avg[col] = (sm_df[col].mean() + fp_df[col].mean()) / 2
+    # get avg and round to the nearest thousandth
+    winner_avg[col] = round((sm_df[col].mean() + fp_df[col].mean()) / 2, 3)
 
-print(winner_avg)
+for key, value in winner_avg.items():
+    print(f'{key}: {value}')
 
 
-# plot the data
+# plot the data for each category
 for col in sm_df.columns:
     if col == 'Year':
         continue
@@ -76,7 +71,7 @@ for col in sm_df.columns:
     plt.axhline(y=winner_avg[col], color='r', linestyle='-', label='Average ' + col)
     plt.xticks(rotation=45)
     plt.xlabel('Year')
-    plt.ylabel('Value')
+    plt.ylabel(col)
     plt.title('Winning ' + col)
     plt.legend()
     plt.show()
--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -6,57 +6,55 @@ files = os.listdir('./dirty_data')
 
 print(f'Found {len(files)} files in the dirty_data folder')
 
-# for each file, analyze
+# for each file, clean
 for file in files:
     print(f'Analyzing {file}')
     data = pd.read_csv(f'./dirty_data/{file}', header=None)
 
-    # Extract the first row and the 13th row to use as part of the column headers
+    # Extract the first row and first two columns
     column_headers = data.iloc[0, :2].tolist()
 
-    # find the row that contains "Batting" as the first column so we can skip it
+    # find the row that contains "Batting" as the first column so we can skip it, this will also imply the number of teams
     index = data[data[0] == 'Batting'].index[0]
-    print(f'Skipping the first {index} rows')
     numTeams = index - 1
     print(f'Number of teams: {numTeams}')
 
-    # Move the first 10 rows to the first two columns
+    # Move the rows with the team names to the first two columns
     teams = data.iloc[1:index, :2]
 
+    # the next row contains the column headers for the next 10 columns
     column_headers.extend(data.iloc[index + 1, :10].tolist())
 
     # if any columns equal 'SVHLD', replace with 'SV'
     column_headers = ['SVHD' if x == 'SV' else x for x in column_headers]
 
-    # # Move the next 10 rows to the next 10 columns
+    # Gather the next numTeams rows as the rotisserie standings stats for the teams
     roti_stats = data.iloc[index + 2:index + numTeams + 2, :10]
 
-    # Concatenate the two parts horizontally
+    # Concatenate the standings with the teams
     roti_table = pd.concat([teams.reset_index(drop=True), roti_stats.reset_index(drop=True)], axis=1)
 
     # Set the column names
     roti_table.columns = column_headers
 
-    # print(roti_table.to_string(index=False))
-
-    # save to csv
+    # save rotisserie standings to csv
     roti_table.to_csv(f'./clean_data/roti/{file.split(' ')[-1]}', index=False)
 
+    # Get the index for where the numerical standings begin
     numerical_stats_index = numTeams * 4 + 10
 
+    # Gather the next numTeams rows as the numerical standings stats for the teams and make those the next 10 columns
     numerical_stats = data.iloc[numerical_stats_index:numerical_stats_index + numTeams, :10]
 
+    # Concatenate the stats with the team names
     numerical_table = pd.concat([teams.reset_index(drop=True), numerical_stats.reset_index(drop=True)], axis=1)
 
+    # Set the column names
     numerical_table.columns = column_headers
-
-    print(numerical_table.to_string(index=False))
 
     if '2015' in file:
         #remove the last 2 rows for data inconsistences
         numerical_table = numerical_table[:-2]
 
-    
-
+    # save numerical standings to csv
     numerical_table.to_csv(f'./clean_data/stats/{file.split(' ')[-1]}', index=False)
-
